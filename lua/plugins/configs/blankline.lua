6b11f54e1dbd1772151c2fcbba69a03d0996ae3e
Til Roth <s8tiroth@stud.uni-saarland.de>
2023-02-16 22:27:03 +0100

    alpha screen

--- lua/plugins/configs/blankline.lua
+++ lua/plugins/configs/blankline.lua
@@ -14,7 +14,7 @@ require('indent_blankline').setup {
     },
     buftype_exclude = { "terminal" },
     show_trailing_blankline_indent = false,
-    show_first_indent_level = false,
+    -- show_first_indent_level = false,
     -- show_current_context = true,
     show_current_context_start = true,
 }
