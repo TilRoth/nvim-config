5d9e50a2e679349e45d2c42f62f9160468b7d5ac
Til Roth <s8tiroth@stud.uni-saarland.de>
2023-02-18 22:05:22 +0100

    terminal navigation

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -13,6 +13,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', 'q', '<C-w>c', opts)
 
 -- Resize with arrows
 vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', opts)
@@ -25,11 +26,12 @@ vim.keymap.set('x', 'J', ':move \'>+1<CR>gv-gv', opts)
 vim.keymap.set('x', 'K', ':move \'<-2<CR>gv-gv', opts)
 
 -- Better terminal navigation
-vim.keymap.set('t', '<esc>', '<C-\\><C-n>', termopts)
+vim.keymap.set('t', '<esc>', '<C-\\><C-n>', term_opts)
 vim.keymap.set('t', '<C-h>', '<C-\\><C-N><C-w>h', term_opts)
 vim.keymap.set('t', '<C-j>', '<C-\\><C-N><C-w>j', term_opts)
 vim.keymap.set('t', '<C-k>', '<C-\\><C-N><C-w>k', term_opts)
 vim.keymap.set('t', '<C-l>', '<C-\\><C-N><C-w>l', term_opts)
+vim.keymap.set('t', '<C-q>', '<C-\\><C-N><C-w>c', term_opts)
 
 -- Stay in indent mode
 vim.keymap.set('v', '<', '<gv', opts)
@@ -45,7 +47,7 @@ vim.keymap.set('n', '<F6>', function() vim.opt.spell = not vim.o.spell end, opts
 vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', opts)
 
 -- Sort visual lines
-vim.keymap.set('v', '<C-s>', ':sort i<CR>', { silent = true })
+vim.keymap.set('v', '<C-s>', ':sort i<CR>', term_opts)
 
 -- Delete trailing whitespaces
 vim.keymap.set('n', '<BS>', ':%s/\\s\\+$//<CR>:w<CR>', opts)
@@ -61,6 +63,7 @@ vim.keymap.set('v', '<C-c>', ":call nerdcommenter#Comment('x', 'toggle')<CR>", o
 
  vim.keymap.set('n', '<F2>', ':TagbarToggle<CR>', opts)
  vim.keymap.set('n', '<F3>', ':NvimTreeToggle<CR>', opts)
+
 
  wk.register({
      name = 'Telescope',
