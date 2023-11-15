b1dcdd86c77af50599238e523f1892de53c86c84
Til Roth <s8tiroth@stud.uni-saarland.de>
2023-11-15 19:03:08 +0100

    update statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,18 @@
+local function lsp_status()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local clients = vim.lsp.buf_get_clients(bufnr);
+
+    if next(clients) == nil then
+        return ''
+    end
+
+    local c = {}
+    for _, client in pairs(clients) do
+        table.insert(c, client.name)
+    end
+    return '\u{f085} ' .. table.concat(c, ' ~ ')
+end
+
 return {
     { 'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -6,8 +21,8 @@ return {
                 options = {
                     icons_enabled = true,
                     theme = require'lualine.themes.everforest',
-                    component_separators = { left = '', right = ''},
-                    section_separators = { left = '', right = ''},
+                    component_separators = { left = '\u{e0bd}', right = '\u{e0b7}'},
+                    section_separators = { left = '\u{e0bc}', right = '\u{e0b6}'},
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
@@ -24,8 +39,8 @@ return {
                 sections = {
                     lualine_a = {'mode'},
                     lualine_b = {'branch', 'diff', 'diagnostics'},
-                    lualine_c = {'filename'},
-                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_c = {'windows'},
+                    lualine_x = {lsp_status, 'filetype'},
                     lualine_y = {'progress'},
                     lualine_z = {'location'}
                 },
